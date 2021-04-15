da00b51b1fac49a4a333650993a8225eb05da1c6
Byron Yego <byronyego@gmail.com>
2021-04-15 14:15:04 +0300

    Loading Data in Home Vue fully

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
     <h3>Hello World</h3>
+    {{pokemons}}
   </div>
 </template>
 
 <script>
+
+import {reactive, toRefs} from "vue";
+
 export default {
   name: 'Home',
-  setup() {
+  setup(){
+
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {}
+  
+    })
+
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
     .then((res)=> res.json())
     .then((data)=> {
       console.log(data)
+      state.pokemons = data.results;
+      state.urlIdLookup = data.results.reduce((acc, cur, idx) => 
+      acc = { ... acc, [cur.name]:idx+1}
+
+      ,{})
     })
+
+    return {... toRefs(state)}
   }
 }
 </script>
