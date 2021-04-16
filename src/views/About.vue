cf993d1df0fee614631c67fa0a11ad9a2a7447c9
Byron Yego <byronyego@gmail.com>
2021-04-16 12:07:24 +0300

    Loading Data in About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,3 +5,27 @@
   
   </div>
 </template>
+
+<script>
+import {useRoute} from "vue-router"
+import {reactive, toRefs} from "vue"
+export default {
+  setup(){
+    const route = useRoute();
+
+    const state = reactive({
+      pokemon: null
+    })
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`)
+        .then((res)=> res.json())
+        .then((data)=> {
+          console.log(data);
+          state.pokemon = data;
+        })
+
+      return {... toRefs(state)}
+  }
+
+}
+</script>
