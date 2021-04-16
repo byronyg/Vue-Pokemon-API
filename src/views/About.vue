42d98a590349183e08ac537cb7c5a5b6978a5ee5
Byron Yego <byronyego@gmail.com>
2021-04-16 12:20:14 +0300

    Updating template in About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <h3>{{$route.params.slug}}</h3>
-  
+   <div v-if="pokemon" class="w-3/12 m-auto bg-purple-100 mt-4 shadow-2xl flex justify-center flex-col items-center">
+     <h3 class="text-2xl text-green-900 uppercase">
+       {{ pokemon.name }}
+     </h3>
+     <div class="flex justify-center">
+       <img class="width-48" :src="pokemon.sprites.front_shiny" alt="">
+       <img class="width-48" :src="pokemon.sprites.back_shiny" alt="">
+     </div>
+     <h3 class="text-yellow-400">Types</h3>
+     <div v-for="(type, idx) in pokemon.types" :key="idx">
+       <h5 class="text-blue-900">{{ type.type.name }}</h5>
+     </div>
+   </div>
   </div>
 </template>
 
